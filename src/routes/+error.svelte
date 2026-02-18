bc7d73aa5c6547e216a1278913febb5d44eba777
hjkl1 <1161524405@qq.com>
2026-02-18 22:35:21 +0800

    add page to present the some error.

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { page } from "$app/state";
+
+    
+</script>
+
+<div class="">
+    <h1 class="text-3xl">An error occurred while processing your request. <span class="font-bold text-retro-error-ink">[{page.error?.message}]</span></h1>
+</div>
