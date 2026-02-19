e7b30398fa50c603094497732cf1a33d08b851ee
hjkl1 <1161524405@qq.com>
2026-02-19 22:23:25 +0800

    add env used to show in Mqrquee bar

--- src/lib/components/Marquee.svelte
+++ src/lib/components/Marquee.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
+    import { PUBLIC_DOMAIN } from "$env/static/public";
     type Props = {
         content?: string;
     };
 
     let {
-        content = "★ Welcome ★ Links updated ★ New tools & textfiles ★ Keep the web small, keep it human ★",
+        content = `★ Welcome to ${PUBLIC_DOMAIN} ★ Links updated ★ New tools & textfiles ★ Keep the web small, keep it human ★`,
     }: Props = $props();
 </script>
 
